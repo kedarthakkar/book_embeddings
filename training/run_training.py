c387341c0033ada93086a9e91a6b3c4241370076
Kedar Thakkar <kedarthakkar@berkeley.edu>
2024-05-26 00:15:05 -0400

    Adds drivers script and runs full epoch training

--- training/run_training.py
+++ training/run_training.py
@@ -57,8 +57,6 @@ if __name__ == "__main__":
     for epoch in range(num_epochs):
         running_loss = 0.0
         for i, row in grouped_interactions.iterrows():
-            if (i + 1) == 1501:
-                break
             user_index = user_to_index[row["user_id"]]
             labels = sparse_tensor[user_index].to(device)
 
