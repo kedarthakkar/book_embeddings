8434a55dbca45ec5cf3e7fdb1e287e924d34de63
Kedar Thakkar <kedarthakkar@berkeley.edu>
2024-05-26 01:18:37 -0400

    break at 1500

--- training/run_training.py
+++ training/run_training.py
@@ -55,6 +55,9 @@ if __name__ == "__main__":
 
     num_epochs = 1
     for epoch in range(num_epochs):
+        if (i + 1) == 1501:
+            break
+
         running_loss = 0.0
         for i, row in grouped_interactions.iterrows():
             user_index = user_to_index[row["user_id"]]
